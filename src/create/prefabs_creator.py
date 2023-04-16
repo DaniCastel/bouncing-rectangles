8639a8d931f92aace5a594a356e83de5aec04d1d
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2023-04-15 21:16:35 -0500

    create_enemy_square and tags

--- src/create/prefabs_creator.py
+++ src/create/prefabs_creator.py
@@ -1,8 +1,11 @@
+import random
 import pygame
 import esper
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_velocity import CVelocity
+from src.ecs.components.tags.c_tag_enemy import CTagEnemy
+from src.ecs.components.tags.c_tag_player import CTagPlayer
 
 
 def create_square(
@@ -15,10 +18,10 @@ def create_square(
     square_entity = ecs_world.create_entity()
 
     ecs_world.add_component(square_entity, CSurface(size, color))
-
     ecs_world.add_component(square_entity, CTransform(position))
-
     ecs_world.add_component(square_entity, CVelocity(velocity))
+
+    return square_entity
 
 
 def create_player_square(
@@ -37,4 +40,28 @@ def create_player_square(
         player_level_config["position"]["x"] - (size.x / 2),
         player_level_config["position"]["y"] - (size.y / 2))
     velocity = pygame.Vector2(0, 0)
-    create_square(world, size, position, velocity, color)
+    player_entity = create_square(world, size, position, velocity, color)
+    world.add_component(player_entity, CTagPlayer)
+
+
+def create_enemy_square(
+        world: esper.World,
+        position: pygame.Vector2,
+        enemy_config: dict):
+
+    size = pygame.Vector2(
+        enemy_config["size"]["x"],
+        enemy_config["size"]["y"])
+    color = pygame.Color(
+        enemy_config["color"]["r"],
+        enemy_config["color"]["g"],
+        enemy_config["color"]["b"])
+
+    velocity_max = enemy_config["velocity_max"]
+    velocity_min = enemy_config["velocity_min"]
+    velocity_range = random.randrange(velocity_min, velocity_max)
+    velocity = pygame.Vector2(
+        random.choice([-velocity_range, velocity_range]),
+        random.choice([-velocity_range, velocity_range]))
+    enemy_entity = create_square(world, size, position, velocity, color)
+    world.add_component(enemy_entity, CTagEnemy)
